876c508ae57cf5b0a91c8083ae851b0fd516a7b7
Peter J Keegan <pjkeeganwh@xtra.co.nz>
2013-12-29 21:52:28 +1300

    update R script comments

--- MaoriStats002.R
+++ MaoriStats002.R
@@ -1,15 +1,15 @@
-### start - Maori Stats 2013  Peter J Keegan  29 Dec 2013 ####
+### start  R script - Maori Stats 2013  Peter J Keegan  29 Dec 2013 ####
 #
 library(ggplot2)
 library(reshape2)
 library(scales)
 #
-# Create vectors of NZ Census Data with addition of 2021 "guestimates"
+# Create vectors of NZ Census Data with addition of my 2021 "guestimates"
 #
 Year <- c(1991,1996,2001,2006,2013,2021)
 Ethnicity <- c(434847,523371,526281,565329,598605,610000)
 Descent <- c(511278,579714,604110,643977,668724,683000)
-Speakers <- c(NA, NA, 130485, 131601,125352,116000)
+Speakers <- c(NA, NA, 130485, 131601,125352,116000) # no language question pre 2001 Census
 #
 # Combine to dataframe
 #
@@ -21,6 +21,8 @@ rm(Descent,Ethnicity, Speakers, Year)  # Get rid of redundant vectors
 #
 d002 <- melt(d001, id.vars="Year")
 names(d002) <- c("Year", "Māori", "Population")
+#
+#  plot
 #
 ggplot(d002, aes(x=Year, y=Population, colour=Māori, group=Māori))+geom_line()+geom_point(size=4)
 #
