e9abbacedda96fd644fab2c84bbaa4a7d63f6d4f
Peter J Keegan <pjkeeganwh@xtra.co.nz>
2013-12-30 21:47:52 +1300

    add rego data

---
+++ MaoriStats004.R
@@ -0,0 +1,13 @@
+### start  R script 004 - Maori Stats 2013  Peter J Keegan  30 Dec 2013 ####
+## New Zealand Census Data, No. of Maori language speakers by Regions, 2001 to 2013 ##
+#
+library(ggplot2)
+library(reshape2)
+#
+d004 <- read.csv(https://raw.github.com/peterjkeegan/MaoriStats01/MSpkReg01.csv", header=T"
+#
+#
+##
+### end of script ###
+
+
