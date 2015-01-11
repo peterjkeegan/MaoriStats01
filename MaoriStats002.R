cd6fbe93d7844c20c5c9673ea0e958fcadb78845
Peter J Keegan <pjkeeganwh@xtra.co.nz>
2015-01-12 10:42:05 +1300

    update

--- MaoriStats002.R
+++ MaoriStats002.R
@@ -1,5 +1,5 @@
 # start  R script #
-# Maori Language stats from NZ Censuses, Peter J Keegan updated 26 Dec 2014
+# Maori Language stats from NZ Censuses, Peter J Keegan updated 12 Jan 2015
 #
 #
 #
@@ -14,7 +14,7 @@ library(scales)
 Year <- c(1991,1996,2001,2006,2013,2021)
 Ethnicity <- c(434847,523371,526281,565329,598605,610000)
 Descent <- c(511278,579714,604110,643977,668724,683000)
-Speakers <- c(NA, NA, 130485, 131601,125352,116000) # no language qu pre 2001
+Speakers <- c(NA, 129048, 130482, 131613,125352,116000) # no language qu pre 1996
 #
 # Combine to dataframe
 #
